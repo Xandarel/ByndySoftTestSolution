f33c81958edd0fcce260838d34e5e726ed5b6135
sbo@friendwork.ru <kbwtbcn1>
2022-05-30 18:02:30 +0500

    commit

---
+++ ByndysoftTestNet/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByndysoftTestNet
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new double[] { double.NaN };
+            Exception exception = null;
+            try
+            {
+                var result = Program.SummMinimumElementOfArray(array);
+            }
+            catch (NotFiniteNumberException e)
+            {
+                exception = e;
+            }
+        }
+
+        public static double SummMinimumElementOfArray(IEnumerable<double> array)
+        {
+            if (array.Count() == 0)
+                throw new NullReferenceException("Пустой массив");
+            if (array == null)
+                throw new NotFiniteNumberException();
+            if (array.Count() == 1)
+                return array.Where(x => !double.IsNaN(x)).Sum();
+            return GetSumm(FindMinElement(array)); 
+        }
+
+        private static double[] FindMinElement(IEnumerable<double> array)
+        {
+            var min1 = array.Where(x => !double.IsNaN(x)).Min();
+            double min2;
+            try
+            {
+                min2 = array.Where(x => !double.IsNaN(x)).Where(x => x != min1).Min();
+            }
+            catch (Exception ex)
+            {
+                min2 = 0;
+            }
+            return new double[] { min1, min2 };
+        }
+
+        private static double GetSumm(IEnumerable<double> array) => array.Sum();
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,77 @@
+using ByndysoftTestNet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EmptyArray()
+        {
+            Exception exception = null;
+            var array = new double[0];
+            try
+            {
+                var result = Program.SummMinimumElementOfArray(array);
+            }
+            catch (NullReferenceException e)
+            {
+                exception = e;
+            }
+            Assert.IsNotNull(exception);
+        }
+        [TestMethod]
+        public void NotInicializeArray()
+        {
+            var array = new double[5];
+            var result = Program.SummMinimumElementOfArray(array);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void InicializeArray()
+        {
+            var array = new double[] { 5, -1, 12, 7, 4 };
+            var result = Program.SummMinimumElementOfArray(array);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void OneElementInArray()
+        {
+            var array = new double[] { 1 };
+            var result = Program.SummMinimumElementOfArray(array);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void OversizeArray()
+        {
+            var array = new double[1000];
+            var rand = new Random();
+            for (int i = 0; i < 1000; i++)
+                array[i] = rand.NextDouble() * i;
+            array[20] = -1;
+            array[400] = -5;
+            Assert.AreEqual(array[20] + array[400], Program.SummMinimumElementOfArray(array));
+        }
+
+        [TestMethod]
+        public void ArrayWithNan()
+        {
+            var array = new double[] { 1, 2, 3, Double.NaN, 4 };
+            Assert.AreEqual(3, Program.SummMinimumElementOfArray(array));
+        }
+        [TestMethod]
+        public void ArrayWithOnlyNaN()
+        {
+            var array = new double[] { double.NaN };
+            Exception exception = null;
+            var result = Program.SummMinimumElementOfArray(array);
+            Assert.AreEqual(0, result);
+        }
+    }
+
+}
